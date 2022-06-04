e6e317e58c5e4b7a12412760c820a84200d5c5a1
Zeke Critchlow <critch646@gmail.com>
2022-06-03 20:45:13 -0700

    demo tk canvas for drawing test targets

--- project/app.py
+++ project/app.py
@@ -1,8 +1,49 @@
+import tkinter as tk
 
-def main():
-    print("project start")
+from tkinter import Canvas
 
 
-if __name__ == "__main__":
-    main()
+app = tk.Tk()
+app.title("Canvas")
+
+canvas = Canvas(app, width=1000, height=1000)
+canvas.pack()
+
+xs = (230, 460, 310, 540)  # 80 units
+s = (335, 455, 425, 545)  # 90 units
+m = (450, 450, 550, 550)  # 100 units
+l = (565, 445, 675, 555)  # 110 units
+xl = (690, 440, 810, 560)  # 120 units
+
+canvas.create_oval(xs, fill="black")
+canvas.create_oval(s, fill="black")
+canvas.create_oval(m, fill="black")
+canvas.create_oval(l, fill="black")
+canvas.create_oval(xl, fill="black")
+
+app.after(1000, app.destroy)
+app.mainloop()
+
+app = tk.Tk()
+app.title("Canvas")
+canvas = Canvas(app, width=1000, height=1000)
+canvas.pack()
+
+canvas.create_oval(450, 50, 550, 150, fill="black")
+canvas.create_oval(450, 850, 550, 950, fill="black")
+
+app.after(1000, app.destroy)
+app.mainloop()
+
+app = tk.Tk()
+app.title("Canvas")
+canvas = Canvas(app, width=1000, height=1000)
+canvas.pack()
+
+canvas.create_oval(50, 450, 150, 550, fill="black")
+canvas.create_oval(850, 450, 950, 550, fill="black")
+
+app.after(1000, app.destroy)
+app.mainloop()
+
 
