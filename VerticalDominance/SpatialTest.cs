3a78b33e644f217ba9f19a06229e63cde808536c
critch646 <critch646@gmail.com>
2022-07-13 07:59:47 -0700

    updated SpatialTest.cs

---
+++ VerticalDominance/SpatialTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerticalDominance
+{
+    internal class SpatialTest
+    {
+        public DateTime _dateTime;
+        public int _participantNumber;
+        public List<TrialBlock>? _trialBlocks;
+    }
+}
