8451c720ebeeb7337b7161d830e38b2507243e7b
critch646 <critch646@gmail.com>
2022-07-13 07:55:05 -0700

    added orientation and stimsize enums

---
+++ VerticalDominance/enums/StimSize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerticalDominance.enums
+{
+    enum StimSize
+    {
+        XS = -2,
+        S = -1,
+        M = 0,
+        L = 1,
+        XL = 2
+    }
+}
