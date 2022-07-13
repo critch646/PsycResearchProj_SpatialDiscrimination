3c05c73b23ca508ba653f832e504254bcbb1e1dd
critch646 <critch646@gmail.com>
2022-07-13 07:59:59 -0700

    created SpatialTrial.cs

---
+++ VerticalDominance/SpatialTrial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerticalDominance
+{
+    internal class SpatialTrial
+    {
+        public int _trialNumber;
+        public enums.Orientation _orientation;
+        public (enums.StimSize, enums.StimSize) _trialType;
+        public int _accuracy;
+        public int _responseTime;
+
+    }
+}
