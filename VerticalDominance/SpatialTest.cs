6e6144426fcad4b55c1d523860831d30c9932f5d
Zeke Critchlow <critch646@gmail.com>
2022-07-21 20:05:57 -0700

    implementing SpatialTest

--- VerticalDominance/SpatialTest.cs
+++ VerticalDominance/SpatialTest.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace VerticalDominance
 {
-    internal class SpatialTest
+    public class SpatialTest
     {
         public DateTime _dateTime;
         public int _participantNumber;
-        public List<TrialBlock>? _trialBlocks;
+        public int _numberOfBlocks;
+        public int _trialsPerBlock;
+        public List<TrialBlock> _trialBlocks;
+
+        public SpatialTest(DateTime dateTime, int participantNumber, int numberOfBlocks, int trialsPerBlock)
+        {
+            this._dateTime = dateTime;
+            this._participantNumber = participantNumber;
+            this._numberOfBlocks = numberOfBlocks;
+            this._trialsPerBlock = trialsPerBlock;
+            this._trialBlocks = new List<TrialBlock>();
+        }
+
+        private void GenerateTest()
+        {
+            for (int i = 1; i <= this._numberOfBlocks; i++)
+            {
+
+            }
+        }
+
     }
 }
