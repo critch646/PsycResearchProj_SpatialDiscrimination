984cdee1cf4ce888491c73f436a162b36fbc50c3
Zeke Critchlow <critch646@gmail.com>
2022-07-21 20:00:14 -0700

    implemented SpatialTrial class

--- VerticalDominance/SpatialTrial.cs
+++ VerticalDominance/SpatialTrial.cs
@@ -6,13 +6,42 @@ using System.Threading.Tasks;
 
 namespace VerticalDominance
 {
-    internal class SpatialTrial
+    public class SpatialTrial
     {
-        public int _trialNumber;
-        public enums.Orientation _orientation;
-        public (enums.StimSize, enums.StimSize) _trialType;
-        public int _accuracy;
-        public int _responseTime;
+        public int TrialNumber { get; set; }
+        public enums.Orientation Orientation { get; private set; }
+        public (enums.StimSize, enums.StimSize) TrialTargets { get; private set; }
+        public int _accuracy = -1;
+        public int _responseTime = -1;
 
+        public SpatialTrial(int trialNumber, enums.Orientation orientation, (enums.StimSize, enums.StimSize) trialTargets)
+        {
+            this.TrialNumber = trialNumber;
+            this.Orientation = orientation;
+            this.TrialTargets = trialTargets;
+        }
+
+        public int Accuracy { 
+            get { return _accuracy; }
+            set 
+            { 
+                if (value >= 0)
+                {
+                    _accuracy = value;
+                }
+            }
+        }
+
+        public int ResponseTime
+        {
+            get { return _responseTime; }
+            set 
+            { 
+                if (value >= 0)
+                {
+                    _responseTime = value;
+                }
+            }
+        }
     }
 }
