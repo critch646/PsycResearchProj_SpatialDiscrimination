03900c1374f700231018cc44f1ec1e4d0b6028ca
Zeke Critchlow <critch646@gmail.com>
2022-07-21 19:59:28 -0700

    created Range struct

---
+++ VerticalDominance/Range.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerticalDominance
+{
+    public struct Range
+    {
+        public int Min, Max;
+
+        public Range(int min, int max)
+        {
+            this.Min = min; 
+            this.Max = max; 
+        }
+    }
+}
