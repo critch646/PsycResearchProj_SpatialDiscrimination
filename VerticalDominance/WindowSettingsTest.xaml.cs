2f74e1b4faa04d6f32df8dc3d1948d66990a0b9f
Zeke Critchlow <critch646@gmail.com>
2022-07-29 16:11:20 -0700

    UI implemented for WindowSettingsTest.xaml

---
+++ VerticalDominance/WindowSettingsTest.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VerticalDominance
+{
+    /// <summary>
+    /// Interaction logic for WindowSettingsTest.xaml
+    /// </summary>
+    public partial class WindowSettingsTest : Window
+    {
+        public WindowSettingsTest()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowSettingsTest_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
