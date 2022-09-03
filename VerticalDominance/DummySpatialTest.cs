2abdc71fa251e29f912dd404a3a448a6f3c8b445
Zeke Critchlow <critch646@gmail.com>
2022-09-03 11:45:28 -0700

    Created DummySpatialTest.cs for testing excel writing

---
+++ VerticalDominance/DummySpatialTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VerticalDominance.enums;
+
+namespace VerticalDominance
+{
+    public class DummySpatialTest
+    {
+        public SpatialTest DummyTest { get; set; }
+
+        DummySpatialTest(int participantID)
+        {
+            this.DummyTest = new SpatialTest(participantID, 4, 25);
+
+            bool testFinished = false;
+
+            while (!testFinished)
+            {
+                if (DummyTest.GetOrientation() == Orientation.horizontal)
+                {
+                    DummyTest.EvaluateResponse(500, System.Windows.Input.Key.Left);
+                } else
+                {
+                    DummyTest.EvaluateResponse(500, System.Windows.Input.Key.Up);
+                }
+
+                testFinished = DummyTest.BumpTest();
+            }
+        }
+    }
+}
